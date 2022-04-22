d7661817c4397539102febee4b53da0f72cda5dc
Prashant <gurungp@Tcd.ie>
2022-04-22 15:51:51 +0100

    Initial Commit

---
+++ DoubleLinkedList/DoubleLinked.h
@@ -0,0 +1,34 @@
+//
+//  DoubleLinked.h
+//  DoubleLinkedList
+//
+//  Created by Prashant Gurung on 22/04/2022.
+//
+
+#ifndef DoubleLinked_h
+#define DoubleLinked_h
+
+struct node{
+  
+    int value;
+    node *prev;
+    node *next;
+    
+};
+
+class DoubleLink
+{
+    node *head=NULL;
+    node *tail=NULL;
+    
+    node *createNode(int val)
+    {
+        if(head==NULL)
+        {
+            
+        }
+    }
+}
+
+
+#endif /* DoubleLinked_h */
---
+++ DoubleLinkedList/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  DoubleLinkedList
+//
+//  Created by Prashant Gurung on 22/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   
+    
+    
+    
+    std::cin.get();
+    return 0;
+}
